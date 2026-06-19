f41e50812c94bb864a6344be2b4747232a97bb57
tangqign99 <tangqign99@github.com>
2026-06-19 20:28:44 +0800

    cleanup CSS + SW v23

---
+++ sw.js
@@ -0,0 +1,38 @@
+/* ==================== Service Worker v23 ==================== */
+var CACHE_NAME = 'avalon-pwa-v23';
+var ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request);
+    })
+  );
+});
